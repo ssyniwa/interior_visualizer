8de8caced4c872bfc2084833ad73f489ee04f7dc
SyouyaNiwa <109267191+ssyniwa@users.noreply.github.com>
2026-08-09 20:57:15 +0900

    Update Japandi path from image to video
    Changed the path of '和モダン (Japandi)' from an image to a video.

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ assets = {
         "北欧風 (Scandinavian)": {"type": "video", "path": "videos/work_scandi.mp4"},
         "モダン (Modern)": {"type": "video", "path": "videos/work_modern.mp4"},
         "インダストリアル (Industrial)": {"type": "video", "path": "videos/work_industrial.mp4"},
-        "和モダン (Japandi)": {"type": "image", "path": "images/work_japandi.jpg"}, # ここを画像に指定
+        "和モダン (Japandi)": {"type": "image", "path": "videos/work_japandi.jpg"}, # ここを画像に指定
     }
 }
 # サイドバー：ユーザーの選択項目
